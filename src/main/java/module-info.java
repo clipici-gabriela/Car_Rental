322eda8ddf2e7667e0aa1352d5052412baf85065
Clipici Maria-Gabriela <101906050+clipici-gabriela@users.noreply.github.com>
2022-05-16 11:20:15 +0300

    CR-12 Create a GUI Login Form

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,15 @@
-module com.example.demo {
+module com.example.carrental {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
+    requires java.sql;
 
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
+    requires mysql.connector.java;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.carrental to javafx.fxml;
+    exports com.example.carrental;
 }
