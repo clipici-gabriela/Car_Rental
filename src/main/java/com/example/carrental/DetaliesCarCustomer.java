d42b0f950f573da1237929053fb77699cc112832
Clipici Maria-Gabriela <101906050+clipici-gabriela@users.noreply.github.com>
2022-05-25 14:00:33 +0300

    Am modifiat codul pentru a se afisa informatiile despre masina selectata.

--- src/main/java/com/example/carrental/DetaliesCarCustomer.java
+++ src/main/java/com/example/carrental/DetaliesCarCustomer.java
@@ -1,22 +1,67 @@
 package com.example.carrental;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
 public class DetaliesCarCustomer implements Initializable {
+
+    @FXML
+    private Label brandLabel;
+
+    @FXML
+    private Label compLabel;
+
+    @FXML
+    private Label dataliesLabel;
+
+    @FXML
+    private Label dateLabel;
+
+    @FXML
+    private Label modelLabel;
+
+    @FXML
+    private Label priceLabel;
+
+
     private String model = CarPageCustomer.car.getModel();
     private String marca = CarPageCustomer.car.getMarca();
+    private String company = CarPageCustomer.company1;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+
+        try {
+            ResultSet rs = connectDB.createStatement().executeQuery("SELECT * FROM cars WHERE company = '"+ company +"' AND marca = '"+ marca +"' AND model = '"+ model +"'");
+
+            while(rs.next()) {
+                brandLabel.setText(rs.getString("marca"));
+                modelLabel.setText(rs.getString("model"));
+                compLabel.setText(rs.getString("company"));
+                dateLabel.setText(rs.getString("an_fabricatie"));
+                priceLabel.setText(rs.getString("pret"));
+                dataliesLabel.setText(rs.getString("detalii"));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
 
     }
 
